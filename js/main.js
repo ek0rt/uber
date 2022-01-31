af73fe64db9cfd521a906c3da1afe0d7261e1792
Kort <de.k0rt@yahoo.de>
2022-01-31 22:28:00 +0300

    uber

---
+++ js/main.js
@@ -0,0 +1,131 @@
+* {
+    font-family: 'Roboto', sans-serif;
+}
+
+nav  {
+    height: 50px;
+    background-color: #212121;
+}
+
+.menu {
+    display: flex;
+    justify-content: space-between;
+    list-style-type: none;
+    padding: 13px 70px;
+    margin: 0;
+
+}
+
+.menu_link {
+    font-size: 15px;
+    font-weight: 300;
+    color: #fff;
+}
+
+.menu_item {
+    border-right: 2px dotted black;
+    padding-right: 33px;
+}
+
+.subheader {
+    height: 84px;
+    background-color: rgba(0, 0, 0, 0.6);
+}
+
+.subheader_logo {
+    display: block;
+    margin-top: 15px;
+}
+
+.subheader_img {
+    max-width: 100%;
+    max-height: 100%;
+}
+
+.subheader_text {
+    margin-top: 4px;
+    font-size: 13px;
+    font-weight: 100;
+    opacity: 0.5;
+    color: rgba(255, 255, 255, 0.5);
+}
+
+.subheader_coll {
+
+    text-align: right;
+    margin-top: 19px;
+    font-size: 12px;
+    font-weight: 300;
+    color: #fff;
+}
+
+.subheader_phone {
+    text-decoration: none;
+    display: block;
+    margin-top: 8px;
+    text-align: right;
+    font-size: 22px;
+    font-weight: 400;
+    color: #fff;
+    text-transform: uppercase;
+    line-height: 12px;
+}
+
+.subheader_btn {
+    margin-top: 19px;
+    width: 172px;
+    height: 46px;
+    box-shadow: inset 0px -3px 0 0 rgba(0, 0, 0, 0.12);
+    border: none;
+    background-image: linear-gradient(to top, #1eacc7, #4ce2ff);
+    font-size: 14px;
+    font-weight: normal;
+    text-align: center;
+    color: #fff;
+    text-transform: uppercase;
+    border-radius: 4px;
+}
+
+.promo {
+    min-height: 800px;
+    background: url('../img/first_list_bg.png') center center/cover no-repeat;
+    padding: 150px 0 135px 0;
+}
+.promo .promo_header {
+    color: #ffffff;
+    font-weight: 700;
+    text-transform: uppercase;
+    font-size: 60px;
+    line-height: 60px;
+    margin: 0;
+    text-align: center;
+}
+.promo .promo_subheader  {
+    color: #ffffff;
+    font-weight: 700;
+    text-transform: uppercase;
+    font-size: 36px;
+    line-height: 36px;
+    margin: 0;
+    margin-top: 22px;
+    text-align: center;
+}
+
+.promo .promo_text {
+    color: #ffffff;
+    font-size: 18px;
+    font-weight: 400;
+    line-height: 24px;
+    text-align: center;
+    margin-top: 23px;
+}
+
+.promo_text {
+    color: #fff;
+    font-size: 18px;
+    font-weight: 400;
+    line-height: 24px;
+    text-align: center;
+    margin-top: 23px;
+
+}
